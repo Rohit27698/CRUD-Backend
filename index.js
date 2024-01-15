f80d2efa6ccae1ce051859facef6abd9848893db
Rohit Kharwar <rohitkumar27699@gmail.com>
2024-01-15 11:18:03 +0530

    done

--- index.js
+++ index.js
@@ -4,15 +4,15 @@ var jwt = require('jsonwebtoken');
 require("dotenv").config()
 const cors = require("cors")
 
-const { connection } = require("./config/db")
-const { todoRouter } = require("./routes/todo.routes")
-const { userRouter } = require("./routes/user.routes")
-const { Authenticate } = require("./middlewares/Authenticate")
+const {Usermodel} = require("./models/User.model")
+const {connection} = require("./config/db")
+const {todoRouter} = require("./routes/todo.routes")
+const {Authenticate} = require("./middlewares/Authenticate")
 
 const app = express();
 
 app.use(cors({
-    origin: "*"
+    origin : "*"
 }))
 
 app.use(express.json())
@@ -20,20 +20,75 @@ app.use(express.json())
 app.get("/", (req, res) => {
     res.send("Base API endpoint")
 })
-app.use("/user", userRouter)
+
+app.post("/signup", async (req, res) => {
+    const {name, email, password, phone} = req.body;
+    try{
+        bcrypt.hash(password, 4, async function(err, hash) {
+            await Usermodel.create({name, email, password : hash, phone})
+            res.send({message : "User successfully created"})
+        });
+    }
+   catch(err){
+    console.log(err)
+    res.status(500).send("Something went wrong, please try again later")
+   }
+})
+
+
+app.get("/check", async (req, res) => {
+   
+    const user = await Usermodel.find()
+    res.send({user});
+})
+ 
+app.post("/login", async (req, res) => {
+    const {email, password} = req.body;
+    const user = await Usermodel.findOne({email})
+    if(!user){
+        return res.send({message : "Sign up first"})
+    }
+    const hash = user?.password
+    bcrypt.compare(password, hash, function(err, result) {
+        if(result){ 
+            const token = jwt.sign({ userID : user._id}, process.env.SECRET_KEY);
+            res.send({message : "login successfull", token : token , user: user})
+        }
+        else{
+            res.send({message : "login failed"})  
+        }
+    });
+})
+app.patch("/login", async (req, res) => {
+    const {name, email, phone,_id} = req.body;
+    const user = await Usermodel.findOne({email})
+    try{
+        await Usermodel.findByIdAndUpdate(_id,{name, email, phone}) 
+        res.send({message : "User Updated "})
+    }
+   catch(err){
+    console.log(err)
+    res.status(500).send("Something went wrong, please try again later")
+   }
+})
+app.get("/checkuser/:id", async (req, res) => {
+    const _id = req.params.id
+    const user = await Usermodel.findOne({_id : _id})
+    res.send({user});
+})
 
 app.use(Authenticate)
-app.use("/todos", todoRouter)
-const PORT = process.env.PORT
+app.use("/todos",todoRouter)
+const PORT=process.env.PORT
 
 app.listen(PORT, async () => {
-    try {
+    try{
         await connection
         console.log("connected to mongodb successfully")
     }
-    catch (err) {
+    catch(err){
         console.log("error connecting to DB")
         console.log(err)
-    }
+    } 
     console.log(`listening on PORT ${PORT}`)
 })
